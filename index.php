5cb077b42a2612c7a39dea00a29fa928a8f51a98
Alexey Nekrasov <alexeynekrasov@MacBook-Pro-Alexey.local>
2020-12-07 14:16:59 -0800

    ver1.1

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ else
     <link rel="stylesheet" href="/styles.css" type="text/css">
 </head>
 <body>
-    <section class="Ты ПИДОР">
+    <section class="CЛЫШ, Ты ПИДОР???">
         <h1>Congratulations!</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
